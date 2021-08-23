a88ea27bf4e5ffbb7c32d4a5a9907d3d4b1a4569
Rember2002 <avelar.remberto@gmail.com>
2021-08-23 09:39:50 -0600

    update Question seeder

--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -19,26 +19,31 @@ class QuestionSeeder extends Seeder
             [
                 'question' => 'Pregunta frecuente numero 1 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum sit assumenda debitis commodi voluptatibus explicabo cupiditate impedit at dolore deleniti nihil, ea officiis vitae autem molestiae quae fugiat saepe ratione.',
                 'answer' => 'Respuesta  de pregunta frecuente numero 1 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum sit assumenda debitis commodi voluptatibus explicabo cupiditate impedit at dolore deleniti nihil, ea officiis vitae autem molestiae quae fugiat saepe ratione.', 
+                'year' => 2021,
             ],
 
             [
                 'question' => 'Pregunta frecuente numero 2 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum sit assumenda debitis commodi voluptatibus explicabo cupiditate impedit at dolore deleniti nihil, ea officiis vitae autem molestiae quae fugiat saepe ratione.',
                 'answer' => 'Respuesta  de pregunta frecuente numero 2 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum sit assumenda debitis commodi voluptatibus explicabo cupiditate impedit at dolore deleniti nihil, ea officiis vitae autem molestiae quae fugiat saepe ratione.', 
+                'year' => 2021,
             ],
 
             [
                 'question' => 'Pregunta frecuente numero 3 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum sit assumenda debitis commodi voluptatibus explicabo cupiditate impedit at dolore deleniti nihil, ea officiis vitae autem molestiae quae fugiat saepe ratione.',
                 'answer' => 'Respuesta  de pregunta frecuente numero 3 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum sit assumenda debitis commodi voluptatibus explicabo cupiditate impedit at dolore deleniti nihil, ea officiis vitae autem molestiae quae fugiat saepe ratione.', 
+                'year' => 2021,
             ],
 
             [
                 'question' => 'Pregunta frecuente numero 4 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum sit assumenda debitis commodi voluptatibus explicabo cupiditate impedit at dolore deleniti nihil, ea officiis vitae autem molestiae quae fugiat saepe ratione.',
                 'answer' => 'Respuesta  de pregunta frecuente numero 4 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum sit assumenda debitis commodi voluptatibus explicabo cupiditate impedit at dolore deleniti nihil, ea officiis vitae autem molestiae quae fugiat saepe ratione.', 
+                'year' => 2021,
             ],
 
             [
                 'question' => 'Pregunta frecuente numero 5 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum sit assumenda debitis commodi voluptatibus explicabo cupiditate impedit at dolore deleniti nihil, ea officiis vitae autem molestiae quae fugiat saepe ratione.',
                 'answer' => 'Respuesta  de pregunta frecuente numero 5 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum sit assumenda debitis commodi voluptatibus explicabo cupiditate impedit at dolore deleniti nihil, ea officiis vitae autem molestiae quae fugiat saepe ratione.', 
+                'year' => 2021,
             ],
 
         ]);
