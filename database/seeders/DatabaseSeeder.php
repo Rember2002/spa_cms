4374df7bfbcff5d8a28c388ae523ac23faf38201
Rember2002 <avelar.remberto@gmail.com>
2021-08-23 09:19:48 -0600

    config field DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\ContactUs;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,41 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $this->call(AboutUsSeeder::class);
+
+        $this->call(AcademicOfferSeeder::class);
+
+        $this->call(CarouselSeeder::class);
+
+        $this->call(CommissionSeeder::class);
+
+        $this->call(ContactUs::class);
+
+        $this->call(GradeSeeder::class);
+
+        $this->call(IntegrantSeeder::class);
+
+        $this->call(NewHomeSeeder::class);
+
+        $this->call(NewItemSeeder::class);
+
+        $this->call(QuestionSeeder::class);
+
+        $this->call(ResourceSeeder::class);
+
+        $this->call(SalesianExperienceHomeSeeder::class);
+
+        $this->call(SalesianExperienceSeeder::class);
+
+        $this->call(SectionNavBarSeeder::class);
+
+        $this->call(ServiceHomeSeeder::class);
+
+        $this->call(ServiceSeeder::class);
+
+        $this->call(StrategicAllieSeeder::class);
+
+        $this->call(StudentCouncilSeeder::class);
     }
 }
