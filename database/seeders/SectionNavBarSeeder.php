e1d0267ff17a32b70862cab50496486ff64192f9
Rember2002 <avelar.remberto@gmail.com>
2021-08-23 09:47:38 -0600

    bug fix SectionNavBar seed

--- database/seeders/SectionNavBarSeeder.php
+++ database/seeders/SectionNavBarSeeder.php
@@ -19,55 +19,46 @@ class SectionNavBarSeeder extends Seeder
             [
                 'name_section' => 'Servicios',
                 'link' => 'url/Servicios',
-                'section_nav_bars' => NULL,
             ],
 
             [
                 'name_section' => 'Estudiantes',
                 'link' => 'url/Estudiantes',
-                'section_nav_bars' => NULL,
             ],
 
             [
                 'name_section' => 'Noticias',
                 'link' => 'url/Noticias',
-                'section_nav_bars' => NULL,
             ],
 
             [
                 'name_section' => 'Donaciones',
                 'link' => 'url/Donaciones',
-                'section_nav_bars' => NULL,
             ],
 
             [
                 'name_section' => 'Acerca de',
                 'link' => 'url/Acerca de',
-                'section_nav_bars' => NULL,
             ],
 
             [
                 'name_section' => 'Vivencia Salesiana',
                 'link' => 'url/Vivencia Salesiana',
-                'section_nav_bars' => NULL,
             ],
 
             [
                 'name_section' => 'Oferta Academica',
                 'link' => 'url/Oferta Academica',
-                'section_nav_bars' => NULL,
             ],
 
             [
                 'name_section' => 'Contactenos',
                 'link' => 'url/Contactenos',
-                'section_nav_bars' => NULL,
             ],
 
             [
                 'name_section' => 'Preguntas Frecuentes',
                 'link' => 'url/Preguntas Frecuentes',
-                'section_nav_bars' => NULL,
             ],
 
         ]);
