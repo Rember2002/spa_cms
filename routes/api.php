4e17c859cdbe8cde658d40be8d3183db2b2b57c4
Rember2002 <avelar.remberto@gmail.com>
2021-08-24 13:47:43 -0600

    made api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,26 @@
 <?php
 
+use App\Http\Controllers\AboutUsController;
+use App\Http\Controllers\AcademicOfferController;
+use App\Http\Controllers\CarouselController;
+use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\CommissionController;
+use App\Http\Controllers\ContactUsController;
+use App\Http\Controllers\GradeController;
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\IntegrantController;
+use App\Http\Controllers\NewHomeController;
+use App\Http\Controllers\NewItemController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\ResourceController;
+use App\Http\Controllers\SalesianExperienceController;
+use App\Http\Controllers\SalesianExperienceHomeController;
+use App\Http\Controllers\SectionNavBarController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\ServiceHomeController;
+use App\Http\Controllers\StrategicAllieController;
+use App\Http\Controllers\StudentCouncilController;
+use App\Models\SectionNavBar;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +38,44 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('aboutus', AboutUsController::class);
+
+Route::apiResource('academicOffers', AcademicOfferController::class);
+
+Route::apiResource('carousels', CarouselController::class);
+
+Route::apiResource('categories', CategorieController::class);
+
+Route::apiResource('commisions', CommissionController::class);
+
+Route::apiResource('contactUs', ContactUsController::class);
+
+Route::apiResource('grades', GradeController::class);
+
+Route::apiResource('images', ImageController::class);
+
+Route::apiResource('integrants', IntegrantController::class);
+
+Route::apiResource('newHomes', NewHomeController::class);
+
+Route::apiResource('newItems', NewItemController::class);
+
+Route::apiResource('questions', QuestionController::class);
+
+Route::apiResource('resources', ResourceController::class);
+
+Route::apiResource('salesianExperiences', SalesianExperienceController::class);
+
+Route::apiResource('salesianExperienceHomes', SalesianExperienceHomeController::class);
+
+Route::apiResource('sectionNavBars', SectionNavBarController::class);
+
+Route::apiResource('services', ServiceController::class);
+
+Route::apiResource('servicesHomes', ServiceHomeController::class);
+
+Route::apiResource('strategicAllies', StrategicAllieController::class);
+
+Route::apiResource('studentCouncils', StudentCouncilController::class);
