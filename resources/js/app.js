358cbdb4e3378522b37474bb3415f349180fa0f2
Rember2002 <avelar.remberto@gmail.com>
2021-08-26 10:41:30 -0600

    init vue in project

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-require('./bootstrap');
+import Vue from 'vue';
+
+import App from './components/App.vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+import 'bootstrap'
+
+const app = new Vue({
+    el: '#app',
+    render: h => h(App)
+});
---
+++ resources/js/components/App.vue
@@ -0,0 +1,6 @@
+<template>
+    <main>
+        <h1>Hello World</h1>
+        <button class="btn btn-primary">hola</button>
+    </main>
+</template>
